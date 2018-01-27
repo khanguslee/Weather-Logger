c90c6e0364ed71910a30a90238310173db9caa08
Kuan Hou Lee <angus.lee809@gmail.com>
2018-01-28 10:53:36 +1100

    Refactor code

---
+++ weather-logger/dataContainer.h
@@ -0,0 +1,15 @@
+
+struct Temperature {
+  float celsius;
+  float fahrenheit;
+};
+
+struct HeatIndex {
+  float celsius;
+  float fahrenheit;
+};
+
+struct Humidity {
+  float percentage;
+};
+
--- weather-logger/weather-logger.ino
+++ weather-logger/weather-logger.ino
@@ -1,8 +1,8 @@
-// Example testing sketch for various DHT humidity/temperature sensors
-// Written by ladyada, public domain
+#include <Timer.h>
+#include "dataContainer.h"
 
+// DHT Options
 #include "DHT.h"
-
 #define DHTPIN 8     // what digital pin we're connected to
 
 // Uncomment whatever type you're using!
@@ -10,50 +10,55 @@
 #define DHTTYPE DHT22   // DHT 22  (AM2302), AM2321
 //#define DHTTYPE DHT21   // DHT 21 (AM2301)
 
-// Initialize DHT sensor.
-// Note that older versions of this library took an optional third parameter to
-// tweak the timings for faster processors.  This parameter is no longer needed
-// as the current DHT reading algorithm adjusts itself to work on faster procs.
 DHT dht(DHTPIN, DHTTYPE);
+Timer timer;
+
+Temperature temperature;
+HeatIndex heatIndex;
+Humidity humidity;
 
 void setup() {
   Serial.begin(9600);
   dht.begin();
+
 }
 
 void loop() {
-  // Wait a few seconds between measurements.
   delay(2000);
+  setTemperature();
+  setHeatIndex();
+  setHumidity();
+}
 
-  // Reading temperature or humidity takes about 250 milliseconds!
-  // Sensor readings may also be up to 2 seconds 'old' (its a very slow sensor)
-  float h = dht.readHumidity();
+void setTemperature() {
   // Read temperature as Celsius (the default)
   float t = dht.readTemperature();
   // Read temperature as Fahrenheit (isFahrenheit = true)
   float f = dht.readTemperature(true);
-
-  // Check if any reads failed and exit early (to try again).
-  if (isnan(h) || isnan(t) || isnan(f)) {
-    Serial.println("Failed to read from DHT sensor!");
-    return;
+  if (!isnan(t)) {
+      temperature.celsius = t;
   }
-
-  // Compute heat index in Fahrenheit (the default)
-  float hif = dht.computeHeatIndex(f, h);
-  // Compute heat index in Celsius (isFahreheit = false)
-  float hic = dht.computeHeatIndex(t, h, false);
-
-  Serial.print("Humidity: ");
-  Serial.println(h);
-  Serial.print("Temperature: ");
-  Serial.print(t);
-  Serial.print(" *C ");
-  Serial.print(f);
-  Serial.println(" *F");
-  Serial.print("Heat index: ");
-  Serial.print(hic);
-  Serial.print(" *C ");
-  Serial.print(hif);
-  Serial.println(" *F");
+  if (!isnan(f)) {
+      temperature.fahrenheit = f;
+  }
 }
+
+void setHeatIndex() {
+  if (!isnan(temperature.celsius) && isnan(humidity.percentage)) {
+      // Compute heat index in Celsius (isFahreheit = false)
+      heatIndex.celsius = dht.computeHeatIndex(temperature.celsius, humidity.percentage, false); 
+  }
+  
+  if (!isnan(temperature.fahrenheit) && isnan(humidity.percentage)) {
+      // Compute heat index in Fahrenheit (the default)
+      heatIndex.fahrenheit = dht.computeHeatIndex(temperature.fahrenheit, humidity.percentage);
+  }
+}
+
+void setHumidity() {
+  float h = dht.readHumidity();
+  if (!isnan(h)) {
+    humidity.percentage = h;
+  }
+}
+
